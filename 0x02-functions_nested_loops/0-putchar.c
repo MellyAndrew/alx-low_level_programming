ae22e709f0324ce5192bf690c1c6fe6ee97ea3ff
MellyAndrew <andy25.melly@gmail.com>
2022-10-19 05:11:19 -0700

    task 0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,23 @@
 #include "main.h"
+
 /**
- * main - print _putchar
- * is written by Andrew
- * Return: 0
+ * main - prints Holberton
+ *
+ * Description: prints Holberton
+ *
+ * Return: 0 (Success)
  */
 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char text[10] = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(text[i]);
+	}
 	_putchar('\n');
+
 	return (0);
+}
