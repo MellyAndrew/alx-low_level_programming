156438ce0c70606d3645904b89036eac1010bb88
MellyAndrew <andy25.melly@gmail.com>
2022-11-01 07:02:27 -0700

    task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
 /**
-  * write your line of code here...
+  * main - Entry point
+  * Description - write your line of code here...
   * Remember:
   * - you are not allowed to use a
   * - you are not allowed to modify p
   * - only one statement
   * - you are not allowed to code anything else than this line of code
+  *
+  * Return: void
   */
 
 int main(void)
